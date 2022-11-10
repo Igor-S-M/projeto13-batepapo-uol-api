f9cb020301d556e98865ec8480226ffe03c5b2e7
Igor-S-M <igorsousa3148@gmail.com>
2022-11-10 04:44:56 -0300

    feat change then/catch to async/await/try/catch

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ import { MongoClient } from "mongodb"
 import dotenv from "dotenv"
 
 
-
 const server = express()
 
 //configs
@@ -23,21 +22,18 @@ mongoClient.connect()
 //coleçoes do db - batePapoUol
 const participants = []
 
-server.get("/participants", (req, res) => {
+server.get("/participants", async (req, res) => {
 
-    db.collection("participants")
-        .find()
-        .toArray()
-        .then(participants => {
-            res.send(participants)
-        }).catch(err => {
-            console.log(err)
-            res.sendStatus(500)
-        })
-
+    try {
+        const promise = await db.collection("participants").find().toArray()
+        res.send(promise.reverse())
+    } catch (err) {
+        console.log(err)
+        res.sendStatus(500)
+    }
 })
 
-server.post("/participants", (req, res) => {
+server.post("/participants", async (req, res) => {
 
     const { name } = req.body
 
@@ -45,41 +41,41 @@ server.post("/participants", (req, res) => {
         res.sendStatus(422)
         return
 
-    } else if (participants.filter(i => i.name === name).length === 0) {
-        res.sendStatus(409)
-        return
+    // } else if (participants.filter(i => i.name === name).length === 0) {
+    //     res.sendStatus(409)
+    //     return
 
     } else {
 
-        db.collection("participants").insert({
-            name,
-            lastStatus: Date.now()
-        })
-            .then((response) => {
-                console.log(response)
-                res.sendStatus(201)
-            }).catch(err => {
-                console.log(err)
+        try {
+            await db.collection("participants").insert({
+                name,
+                lastStatus: Date.now()
             })
+            res.sendStatus(201)
+
+        } catch (err) {
+            res.sendStatus(500)
+        }
 
     }
 })
 
-server.get("/messages", (req, res) => {
+server.get("/messages", async (req, res) => {
 
-    db.collection("messages")
-        .find()
-        .toArray()
-        .then(messages => {
-            res.send(messages)
-        }).catch(err => {
-            console.log(err)
-            res.sendStatus(500)
-        })
+    try {
+        const promise = await db.collection("messages").find().toArray()
+        res.send(promise.reverse())
+
+    } catch (err) {
+        console.log(err)
+        res.sendStatus(500)
+
+    }
 
 })
 
-server.post("/messages", (req, res) => {
+server.post("/messages", async (req, res) => {
 
     const { to, text, type } = req.body
     const { user } = req.headers
@@ -91,28 +87,28 @@ server.post("/messages", (req, res) => {
         // } else if (type !== "message" || type !== "private_message") {
         //     res.status(422).send("errou no type")
         //     return
-    // } else if (!participants.find(i => i.name === user)) {
-    //     res.status(422).send("errou no participants")
-    //     return
+        // } else if (!participants.find(i => i.name === user)) {
+        //     res.status(422).send("errou no participants")
+        //     return
     } else {
 
         const seg = (dayjs().second())
         const min = (dayjs().minute())
         const hora = (dayjs().hour())
 
-        db.collection("messages").insert({
-            from: user,
-            to,
-            text,
-            type,
-            time: `${hora}:${min}:${seg}`
-        })
-            .then((response) => {
-                console.log(response)
-                res.sendStatus(201)
-            }).catch(err => {
-                console.log(err)
+        try {
+            await db.collection("messages").insert({
+                from: user,
+                to,
+                text,
+                type,
+                time: `${hora}:${min}:${seg}`
             })
+            res.sendStatus(201)
+
+        } catch (err) { 
+            res.sendStatus(500)
+        }
 
     }
 })
